2f9a9b2e07704e87f3ea507054d3ce59a2ad072f
YuriyDobranyuk <dobranyuk@gmail.com>
2022-07-07 14:15:58 +0300

    Make visual part for main page

---
+++ CryptoApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoApp.ViewModels.Base;
+
+namespace CryptoApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Program crypto manager";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+    }
+}
